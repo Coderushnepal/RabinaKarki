040e76135bdb78a1f7e33305219df6f5d44b441b
Rabs <karkirabu@gmail.com>
2022-01-03 17:40:43 +0545

    comment update

--- JS assignments/fizz.py
+++ JS assignments/fizz.py
@@ -1,6 +1,7 @@
-#Write a short program that prints each number from 1 to 100 on a new line. 
- #For each multiple of 3, print "Fizz" instead of the number. 
- 
+"""
+Write a short program that prints each number from 1 to 100 on a new line. 
+For each multiple of 3, print "Fizz" instead of the number. 
+"""
 for number in range(1,101):
   if number%3==0:
     print("Fizz")  
